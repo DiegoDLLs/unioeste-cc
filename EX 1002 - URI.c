9d69dfd8770e2bb939c2756547feb7996271dd23
DiegoDLLs <49327636+DiegoDLLs@users.noreply.github.com>
2019-04-11 19:13:32 -0300

    Create EX 1002 - URI.c

---
+++ EX 1002 - URI.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#define pi 3.14159
+
+int main()
+{
+    double R, A;
+    scanf("%lf", &R);
+    A = pi*pow(R,2);
+    printf("A=%.4lf\n", A);
+    return 0;
+}
